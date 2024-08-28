e2ac8f9effd769f6e5a9ed659f72cf726824a08c
Debiprasad Mohanty <145470177+Debiprasadmohanty18@users.noreply.github.com>
2024-08-28 21:19:59 +0530

    Add files via upload

---
+++ IndexOfFirstOccuranceInString.java
@@ -0,0 +1,24 @@
+
+public class IndexOfFirstOccuranceInString {
+
+	public static void main(String[] args) {
+		IndexOfFirstOccuranceInString ob = new IndexOfFirstOccuranceInString();
+		System.out.println(ob.strStr("code", "codeee"));
+	}
+	
+	public int strStr(String haystack, String needle)
+	{
+		for(int i=0;i<haystack.length() - needle.length()+1;i++)
+		{
+			if(haystack.charAt(i) == needle.charAt(0))
+			{
+				if(haystack.substring(i, needle.length() + i).equals(needle))
+				{
+					return i;
+				}
+			}
+		}
+		return -1;
+	}
+
+}
