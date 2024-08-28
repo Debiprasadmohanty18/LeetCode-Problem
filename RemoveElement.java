f4563ab0141c5b4c96d80dcd68f2142102c7cba3
Debiprasad Mohanty <145470177+Debiprasadmohanty18@users.noreply.github.com>
2024-08-28 18:56:53 +0530

    Add files via upload

---
+++ RemoveElement.java
@@ -0,0 +1,25 @@
+
+public class RemoveElement {
+	public static void main(String[] args) {
+		RemoveElement obj = new RemoveElement();
+		int [] nums = {0,1,2,2,3,0,4,2};
+		System.out.println(obj.removeElement(nums, 2));
+	}
+	
+	public int removeElement(int [] nums, int val)
+	{
+		int arrValue = 0;
+		for(int i=0;i<nums.length;i++)
+		{
+			if(nums[i] != val)
+			{
+				nums[arrValue] = nums[i];
+				System.out.print(nums[arrValue] + " ");
+				arrValue++;
+			}
+		}
+		System.out.println();
+		return arrValue;
+		
+	}
+}
