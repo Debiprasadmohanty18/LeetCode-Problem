5e0d939c58e4a552b29474d7f2f576aaf2a04237
Debiprasad Mohanty <145470177+Debiprasadmohanty18@users.noreply.github.com>
2024-08-29 15:05:28 +0530

    Add files via upload

---
+++ LengthOfLastWord.java
@@ -0,0 +1,24 @@
+
+public class LengthOfLastWord {
+	public static void main(String[] args) {
+		String s = "   fly me   to   the moon  ";
+		System.out.println(lengthOfLastWord(s));
+	}
+	
+	static int lengthOfLastWord(String s)
+	{
+		int countChar = 0;
+		for(int i=s.length()-1;i>=0;i--)
+		{
+			if(s.charAt(i) != ' ')
+			{
+				countChar++;
+			}
+			else if(s.charAt(i) == ' ' && countChar != 0)
+			{
+				break;
+			}
+		}
+		return countChar;
+	}
+}
