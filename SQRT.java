be045c39e4b62e5fe57193e98db847613033c9d1
Debiprasad Mohanty <145470177+Debiprasadmohanty18@users.noreply.github.com>
2024-08-30 18:26:46 +0530

    Add files via upload

---
+++ SQRT.java
@@ -0,0 +1,33 @@
+
+public class SQRT {
+	public static void main(String[] args) {
+		int x = 8;
+		System.out.println(sqrtNum(x));
+	}
+	
+	static int sqrtNum(int x)
+	{
+		int start = 1;
+		int end = x;
+		int answer = 0;
+		while(start <= end)
+		{
+			int mid = start + (end-start)/2;
+			if((mid*mid) == x)
+			{
+				answer = (int)mid;
+				break;
+			}
+			else if((mid*mid) < x)
+			{
+				start = mid + 1;
+				answer = mid;
+			}
+			else
+			{
+				end = mid -1;
+			}
+		}
+		return (int)answer;
+	}
+}
