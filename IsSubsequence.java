324eb25ca882002c31c0f6ff8844fc69707d3df4
Debiprasad Mohanty <145470177+Debiprasadmohanty18@users.noreply.github.com>
2024-08-31 21:50:43 +0530

    Add files via upload

---
+++ IsSubsequence.java
@@ -0,0 +1,28 @@
+
+public class IsSubsequence {
+
+	public static void main(String[] args) {
+		String s = "abef";
+		String t = "abcdef";
+		System.out.println(isSubsequence(s, t));
+	}
+	
+	static boolean isSubsequence(String s,String t)
+	{
+		int countS = 0;
+		int countT = 0;
+		while(countS<s.length() && countT<t.length()) 
+		{
+			if(s.charAt(countS) == t.charAt(countT))
+			{
+				countS++;
+			}
+			countT++;
+		}
+		if (countS == s.length()) 
+		{
+			return true;
+		}	
+		return false;
+	}
+}
