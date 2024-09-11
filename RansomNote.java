446067b1cfddd99de35d14b188bd9e53ceeb6d0b
Debiprasad Mohanty <145470177+Debiprasadmohanty18@users.noreply.github.com>
2024-09-11 20:34:34 +0530

    Add files via upload

---
+++ RansomNote.java
@@ -0,0 +1,30 @@
+
+public class RansomNote {
+
+	public static void main(String[] args) {
+		String ransomNote = "aa";
+		String magazine = "aaab";		
+		System.out.println(canConstruct(ransomNote, magazine));
+	}
+	
+	public static boolean canConstruct(String ransomNote, String magazine) 
+	{
+				
+		int[] count = new int[26];
+		for(char ch : magazine.toCharArray())
+		{
+			count[ch - 'a']++;
+		}
+		for(char ch : ransomNote.toCharArray())
+		{
+			count[ch - 'a']--;
+			
+			if(count[ch - 'a'] < 0)
+			{
+				return false;
+			}
+		}
+		return true;
+	}
+
+}
