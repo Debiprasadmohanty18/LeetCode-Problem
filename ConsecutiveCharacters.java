2cf376abda539214a11063dec2e5c5edf9c90f59
Debiprasad Mohanty <145470177+Debiprasadmohanty18@users.noreply.github.com>
2024-09-13 20:19:17 +0530

    Add files via upload

---
+++ ConsecutiveCharacters.java
@@ -0,0 +1,38 @@
+
+public class ConsecutiveCharacters {
+
+	public static void main(String[] args) 
+	{
+		String s = "aabbccxtbdadd";
+		System.out.println(maxCount(s));
+	}
+
+	public static int maxCount(String s)
+	{
+		int i = 0,j = 1;
+		int x = 1;
+		int ans = 1;
+		while(i<s.length()-1)
+		{
+			if(s.charAt(i) == s.charAt(j))
+			{
+				x++;
+				i++;
+				j++;
+				if(x > ans)
+				{
+					ans = x;
+				}
+			}
+			else 
+			{
+				
+				i++;
+				j++;
+				x = 1;
+			}
+			
+		}
+		return ans;
+	}
+}
