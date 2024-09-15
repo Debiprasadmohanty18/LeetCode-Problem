076e75648e958e1c634fb24f84b96873890beb36
Debiprasad Mohanty <145470177+Debiprasadmohanty18@users.noreply.github.com>
2024-09-15 20:46:13 +0530

    Add files via upload

---
+++ WordPattern.java
@@ -0,0 +1,41 @@
+import java.util.HashMap;
+
+public class WordPattern {
+
+	public static void main(String[] args) 
+	{
+		String pattern = "aaa";
+		String s = "aa aa aa aa";
+		System.out.println(wordPattern(pattern, s));
+	}
+	
+	public static boolean wordPattern(String pattern, String s)
+	{
+		char[] ch = pattern.toCharArray();
+		String[] t = s.split(" ");
+		HashMap<Character, String> map = new HashMap<>();
+		if(ch.length != t.length)
+		{
+			return false;
+		}
+		for(int i=0;i<pattern.length();i++)
+		{
+			if(map.containsKey(ch[i]))
+			{
+				if(!(map.get(ch[i]).equals(t[i])))
+				{
+					return false;
+				}
+			}
+			else 
+			{
+				if(map.containsValue(t[i]))
+				{
+					return false;
+				}
+				map.put(ch[i], t[i]);
+			}			
+		}
+		return true;
+	}
+}
