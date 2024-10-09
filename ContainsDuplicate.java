49f01582546168608669a08fa7de9c60857e176f
Debiprasad Mohanty <145470177+Debiprasadmohanty18@users.noreply.github.com>
2024-10-09 16:13:48 +0530

    Add files via upload

---
+++ ContainsDuplicate.java
@@ -0,0 +1,38 @@
+package com.tanmay.LeetCodeEasyProblems;
+
+import java.util.Arrays;
+
+public class ContainsDuplicate {
+
+	public static void main(String[] args) {
+		int[] nums = {1,2,3,4};
+		System.out.println(containsDuplicate(nums));
+	}
+	
+	public static boolean containsDuplicate(int[] nums) {
+/*		for(int i=0;i<nums.length;i++)
+		{
+			for(int j=0;j<nums.length;j++)
+			{
+				if(i == j)
+				{
+					continue;
+				}
+				else if (nums[i] == nums[j]) 
+				{
+					return true;
+				}
+			}
+		}
+        return false;  */
+		
+		Arrays.sort(nums);
+		for(int i=0;i<nums.length-1;i++)
+		{
+			if(nums[i] == nums[i+1])
+				return true;
+		}
+		return false;
+    }
+
+}
