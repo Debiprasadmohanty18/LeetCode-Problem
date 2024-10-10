f3023388bc69746b3cb3aae51a0833c5291118da
Debiprasad Mohanty <145470177+Debiprasadmohanty18@users.noreply.github.com>
2024-10-10 17:52:13 +0530

    Add files via upload

---
+++ MissingNumber.java
@@ -0,0 +1,34 @@
+package com.tanmay.LeetCodeEasyProblems;
+
+public class MissingNumber {
+
+	public static void main(String[] args) {
+		int[] nums = {1,0,3,4};
+		System.out.println(missingNumber(nums));
+	}
+	
+	public static int missingNumber(int[] nums) {
+        // Arrays.sort(nums);
+        // for(int i=0;i<nums.length;i++)
+        // {
+        //     if(nums[i] == i)
+        //     {
+        //         continue;
+        //     }
+        //     else
+        //     {
+        //         return i;
+        //     }
+        // }
+        // return nums.length;
+        int n = nums.length;
+
+        int sum = n*(n+1)/2; // sum of numbers 0 ~ n
+
+        for(int num : nums){
+            sum -= num;
+        } 
+        return sum;
+    }
+
+}
