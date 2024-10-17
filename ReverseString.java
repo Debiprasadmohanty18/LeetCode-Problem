f0b2cf36793eae866f0356ffe8ac6392d7c73a2c
Debiprasad Mohanty <145470177+Debiprasadmohanty18@users.noreply.github.com>
2024-10-17 15:00:34 +0530

    Add files via upload

---
+++ ReverseString.java
@@ -0,0 +1,25 @@
+package com.tanmay.LeetCodeEasyProblems;
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+		char[] s = {'h','e','l','l','o'};
+		reverseString(s);
+	}
+	
+	static void reverseString(char[] s) 
+	{
+		int i = 0;
+		int size = s.length-1;
+		while(i<=size)
+		{
+			char temp = s[size];
+			s[size] = s[i];
+			s[i] = temp;
+			size--;
+			i++;	
+		}
+		System.out.println(s);
+	}
+
+}
