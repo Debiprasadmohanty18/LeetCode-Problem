4d5e1730c540d4c53577d57ffbd2defc36ef661e
Debiprasad Mohanty <145470177+Debiprasadmohanty18@users.noreply.github.com>
2024-10-28 22:58:55 +0530

    Add files via upload

---
+++ LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,32 @@
+package com.tanmay.LeetCodeEasyProblems;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LongestSubstringWithoutRepeatingCharacters {
+	public static void main(String[] args) {
+		String s = "abcabcbb";
+		System.out.println(lengthOfLongestSubstring(s));
+	}
+	
+	public static int lengthOfLongestSubstring(String s) {
+        Map<Character,Integer> ss = new HashMap<Character, Integer>();
+        int start = 0, len = 0;
+        
+        for(int end=0;end<s.length();end++)
+        {
+        	char ch = s.charAt(end);
+        	
+        	if(ss.containsKey(ch))
+        	{
+        		if(start<=ss.get(ch))
+        		{
+        			start = ss.get(ch)+1;
+        		}
+        	}
+    		len = Math.max(len, end-start+1);
+			ss.put(ch, end);
+        }
+        return len;
+    }
+}
