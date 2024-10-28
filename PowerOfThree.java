0827c8ac3732a48b935996a96e2435dfbbd21c0c
Debiprasad Mohanty <145470177+Debiprasadmohanty18@users.noreply.github.com>
2024-10-28 23:16:34 +0530

    Add files via upload

---
+++ PowerOfThree.java
@@ -0,0 +1,24 @@
+package com.tanmay.LeetCodeEasyProblems;
+
+public class PowerOfThree {
+
+	public static void main(String[] args) 
+	{
+		int n = 28;
+		System.out.println(isPowerOfThree(n));
+		
+	}
+	
+	static boolean isPowerOfThree(int n)
+	{
+		while(n>=3)
+		{
+			if(n%3 != 0)
+				return false;
+			
+			n=n/3;
+		}
+		return n==1;
+	}
+
+}
