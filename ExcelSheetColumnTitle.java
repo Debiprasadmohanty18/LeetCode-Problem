683428d684f5046df8d81a448465fb4010236976
Debiprasad Mohanty <145470177+Debiprasadmohanty18@users.noreply.github.com>
2024-10-30 22:23:45 +0530

    Update ExcelSheetColumnTitle.java

--- ExcelSheetColumnTitle.java
+++ ExcelSheetColumnTitle.java
@@ -1,9 +1,7 @@
 /* Excel Sheet Column Title :- 
 
 Given an integer columnNumber, return its corresponding column title as it appears in an Excel sheet.
-
 For example:
-
 A -> 1
 B -> 2
 C -> 3
@@ -12,18 +10,16 @@ Z -> 26
 AA -> 27
 AB -> 28 
 ...
- 
 
 Example 1:
-
 Input: columnNumber = 1
 Output: "A"
-Example 2:
 
+Example 2:
 Input: columnNumber = 28
 Output: "AB"
-Example 3:
 
+Example 3:
 Input: columnNumber = 701
 Output: "ZY"
  
