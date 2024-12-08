fb04f216d1504420fac77ad0e35b1f2ed333f3a6
Debiprasad Mohanty <145470177+Debiprasadmohanty18@users.noreply.github.com>
2024-12-08 19:06:59 +0530

    Add files via upload

---
+++ NumberOfGoodPairs.java
@@ -0,0 +1,31 @@
+package Array;
+
+public class NumberOfGoodPairs {
+
+	public static void main(String[] args) {
+		
+		int[] nums = {1,2,3,1,1,3};
+		System.out.println(numIdenticalPairs(nums));
+
+	}
+	
+	static int numIdenticalPairs(int[] nums) 
+	{
+       int count = 0;
+       for(int i=0;i<nums.length;i++)
+       {
+    	   for(int j=i;j<nums.length;j++)
+    	   {
+    		   if(i == j)
+    			   continue;
+    		   else if (nums[i] == nums[j]) 
+    		   {
+    			   count++;   			   
+    		   }
+    	   }
+       }
+       
+       return count;
+    }
+
+}
