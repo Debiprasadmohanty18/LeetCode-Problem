f980d51eaec2418a3b941699bec83bd14d0d5638
Debiprasad Mohanty <145470177+Debiprasadmohanty18@users.noreply.github.com>
2024-12-13 20:57:20 +0530

    Update TransposeMatrix.java

--- TransposeMatrix.java
+++ TransposeMatrix.java
@@ -1,4 +1,4 @@
-/*
+/* Transpose Matrix :-
 Given a 2D integer array matrix, return the transpose of matrix.
 The transpose of a matrix is the matrix flipped over its main diagonal, switching the matrix's row and column indices.
 
