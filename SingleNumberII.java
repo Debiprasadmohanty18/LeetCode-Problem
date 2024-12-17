ae6d44e3f69fa548322311271a7bb575bebc8a95
Debiprasad Mohanty <145470177+Debiprasadmohanty18@users.noreply.github.com>
2024-12-17 21:59:09 +0530

    Add files via upload

---
+++ SingleNumberII.java
@@ -0,0 +1,29 @@
+package com.tanmay.LeetCodeEasyProblems;
+
+public class SingleNumberII {
+
+	public static void main(String[] args) {
+		int[] nums = {2,2,3,2};
+		System.out.println(singleNumber(nums));
+	}
+	
+	static int singleNumber(int[] nums) 
+	{
+        for(int i=0;i<nums.length;i++)
+        {
+        	int count = 0;
+        	for(int j=0;j<nums.length;j++)
+        	{
+        		if(i == j)
+        			continue;
+        		if(nums[i] == nums[j])
+        			count++;
+        	}
+        	if(count == 0)
+        		return nums[i];
+        }
+        
+        return -1;
+    }
+
+}
